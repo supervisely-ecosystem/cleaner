b2aa24c1b59bbf5e3dda3ced06dd3aa7e3ea95c3
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-01-20 17:48:15 +0400

    Update sly_functions.py

--- src/sly_functions.py
+++ src/sly_functions.py
@@ -169,8 +169,6 @@ def clean_offline_sessions(
 
         if len(task_infos) > 0:
             task_ids_to_remove.update({t["id"] for t in task_infos if is_removable(t, app_names)})
-        else:
-            task_ids_to_remove = []
         
         file_to_del_paths = []
         for file_info in files_infos:
