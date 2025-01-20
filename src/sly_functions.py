6fa55bbd81eb8ea2008b2ac4a95681ba4f2d174a
Pavel Bartsits <48913536+cxnt@users.noreply.github.com>
2025-01-20 18:26:46 +0400

    Add empty checks for task listing (#12)
    * Add checks
    
    * Update sly_functions.py
    
    * Update sly_functions.py
    
    * Add batch tasks
    
    * Remove unnecessary filters initialization in clean_offline_sessions function

--- src/sly_functions.py
+++ src/sly_functions.py
@@ -157,13 +157,20 @@ def clean_offline_sessions(
         scanned_files += len(files_infos)
 
         all_task_ids = {get_task_id(file_info["path"]) for file_info in files_infos}
+        if all_task_ids:
+            if w_ids:
+                task_infos = []
+                for batch_tasks in sly.batched(all_task_ids, 500):
+                    filters = [{"field": "id", "operator": "in", "value": list(batch_tasks)}]
+                    task_infos.extend([t for w_id in w_ids for t in api.task.get_list(w_id, filters)])
+            else:
+                task_infos = []
+        else:
+            task_infos = []
 
-        filters = [{"field": "id", "operator": "in", "value": list(all_task_ids)}]
-        task_infos = [t for w_id in w_ids for t in api.task.get_list(w_id, filters)]
-        task_ids_to_remove.update(
-            {t["id"] for t in task_infos if is_removable(t, app_names)}
-        )
-
+        if len(task_infos) > 0:
+            task_ids_to_remove.update({t["id"] for t in task_infos if is_removable(t, app_names)})
+        
         file_to_del_paths = []
         for file_info in files_infos:
             if get_task_id(file_info["path"]) in task_ids_to_remove:
